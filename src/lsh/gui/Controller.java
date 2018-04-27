da4e9e1ecb92532b61bf9fdfbeb79d549843ed81
LBYPatrick <lbypatrick@gmail.com>
2018-04-27 14:28:01 -0400

    Update GUI

--- src/lsh/gui/Controller.java
+++ src/lsh/gui/Controller.java
@@ -1,20 +1,28 @@
 package lsh.gui;
 
 import javafx.collections.FXCollections;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import lsh.backend.Core;
 import lsh.backend.Database;
 import lsh.backend.Utils;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.util.ArrayList;
 
 public class Controller {
 
     public ChoiceBox solution_box;
     public ProgressBar pBar;
+    public ImageView Logo;
+
 
     public void initSolutionBox() {
         ArrayList<String> solution_urls = new ArrayList<>();
@@ -27,7 +35,15 @@ public class Controller {
         solution_box.getSelectionModel().select(0);
 
         Utils.setVisualizing(false);
+    }
 
+    private Image getImage(String filePath) {
+        try {
+            return new Image(new File(filePath).toURI().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @FXML
@@ -37,6 +53,9 @@ public class Controller {
 
         //Initialize Solution Box
         initSolutionBox();
+
+        //Initialize Logo
+        Logo.setImage(getImage("../../../../resources/lshelper_full.png"));
     }
 
     public void runSolution(MouseEvent mouseEvent) {
--- src/lsh/gui/FixThread.java
+++ src/lsh/gui/FixThread.java
@@ -1,5 +1,6 @@
 package lsh.gui;
 
+import javafx.application.Platform;
 import javafx.scene.control.ProgressBar;
 import lsh.backend.Core;
 
@@ -17,6 +18,27 @@ public class FixThread extends Thread {
 
     @Override
     public void run() {
+        new Thread(() -> {
+            try {
+                Platform.runLater(() -> bar.setStyle("-fx-accent:blue "));
+                Platform.runLater(() -> bar.setProgress(0));
+
+                while (stat.current < stat.total) {
+                    Platform.runLater(() -> bar.setProgress((((double) stat.current) / ((double) stat.total))));
+
+                    //System.out.printf(status.current + "\t" + status.total + "\n"); //Just for Debug
+
+                    Thread.sleep(100); //10 Times per second -- Can't be too fast
+                }
+
+                Platform.runLater(() -> bar.setProgress(1));
+                Platform.runLater(() -> bar.setStyle("-fx-accent: green "));
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
         new ProgressBarThread(bar,stat).start();
         Core.RunFix(fixNumber,stat);
     }
